288b7b16142d7768a8e91e61a0e9c48c8fd86087
Me 246402 <246402@cloveland.nfshost.com>
2017-01-23 05:00:32 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Calvin Loveland</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+    <div id="main">
+
+    <?php include("header.html");?>
+        <div id="title">
+            <img id="profile" src="me_irl.jpg"/>
+                <span class="title-line" style="text-decoration: underline;">Calvin Loveland</span>
+            <hr style="width:75%; color: #0e1717;">
+            <p>I am a computer science student at Utah State University</p>
+            <p>I've done work in almost every area of development</p>
+        </div>
+    <p id="disclaimer">UNDER CONSTRUCTION</p>
+    </body>
+
+    </div>
+</html>
