901ac773238f80bcc4c869e2f2fa6e892d66aead
Calvin Loveland <calvinloveland@gmail.com>
2017-01-22 23:32:51 -0700

    Revert "Revert "Added line to index.php""
    This reverts commit 17aa0199f9fe269a8c45c1c2389bc401abcb0bf5.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
                 <span class="title-line" style="text-decoration: underline;">Calvin Loveland</span>
             <hr style="width:75%; color: #0e1717;">
             <p>I am a computer science student at Utah State University</p>
-            <p>I've done work in almost every area of development</p>
+            <p>Experienced with C#, C++, Java, HTML, PHP, and CSS</p>
         </div>
     <p id="disclaimer">UNDER CONSTRUCTION</p>
     </body>
