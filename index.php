3342d5c981abf8b549695f2161f3c6de999dc162
Calvin Loveland <Calvin Loveland>
2017-04-03 22:27:48 -0600

    added viewport meta tag and expanded intro

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Calvin Loveland</title>
         <link rel="stylesheet" type="text/css" href="styles.css">
     </head>
@@ -12,6 +13,8 @@
                 <span class="title-line" style="text-decoration: underline;">Calvin Loveland</span>
             <hr style="width:75%; color: #0e1717;">
             <p>I am a computer science student at Utah State University</p>
+            <p>I'm currently looking for a summer job take a look at my resume.</p>
+            <P>If you know of a position I could fill don't hesitate to contact me!</P>
             <h3>Experienced in:</h3>
             <p>(In descending order of proficiency)</p>
             <?php include("experience.html");?>
